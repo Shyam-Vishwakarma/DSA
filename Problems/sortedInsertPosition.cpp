e3283ac5716c81a9c3d7c3ec7cfcb8120d4a39e8
Shyam Vishwakarma <shyamvswkrma@gmail.com>
2023-09-15 12:31:06 +0530

    solved sorted insert position

---
+++ Problems/sortedInsertPosition.cpp
@@ -0,0 +1,22 @@
+//https://www.interviewbit.com/problems/sorted-insert-position/
+
+#include<iostream>
+#include<vector>
+using namespace std;
+
+//using binary search :
+int searchInsert(vector<int> &A, int B) {
+    int low=0, high=A.size()-1;
+    while(low<=high) {
+        int mid=low+(high-low)/2;
+        if(A[mid]==B) {
+            return mid;
+        }
+        else if(A[mid]>B) {
+            high=mid-1;
+        } else {
+            low=mid+1;
+        }
+    }
+    return low;
+}
