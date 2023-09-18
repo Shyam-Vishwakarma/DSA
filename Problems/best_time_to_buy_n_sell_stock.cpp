2e5ae67b7b6af0d3f10f159476efee9d914d1275
Shyam Vishwakarma <shyamvswkrma@gmail.com>
2023-09-18 10:32:37 +0530

    solved problem 121(leetcode)

---
+++ Problems/best_time_to_buy_n_sell_stock.cpp
@@ -0,0 +1,17 @@
+//https://leetcode.com/problems/best-time-to-buy-and-sell-stock/
+
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int  profit=0;
+        int cheaperPrice=prices[0];
+        for(int i=1; i<prices.size(); i++) {
+            if(prices[i]<cheaperPrice) {
+                cheaperPrice=prices[i];
+            } else if(prices[i]>cheaperPrice) {
+                if(prices[i]-cheaperPrice>profit) profit=prices[i]-cheaperPrice;
+            }
+        }
+        return profit;
+    }
+};
