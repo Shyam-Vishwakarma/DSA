5aa7da6828d637177a03502ba2c6c48a73deef76
Shyam Vishwakarma <shyamvswkrma@gmail.com>
2023-09-21 11:48:41 +0530

    solved first_n_last_position

---
+++ Problems/Binary Search/first_n_last_position.cpp
@@ -0,0 +1,35 @@
+// https://leetcode.com/problems/find-first-and-last-position-of-element-in-sorted-array/description/
+
+class Solution {
+public:
+    vector<int> searchRange(vector<int>& nums, int target) {
+        int low=0, high=nums.size()-1, mid;
+        int firstPosition=-1, lastPosition=-1;
+        // first position
+        while(low<=high) {
+            mid=low+(high-low)/2;
+            if(nums[mid]==target) {
+                firstPosition=mid;
+                high=mid-1;
+            } else if(nums[mid]>target) {
+                high=mid-1;
+            } else {
+                low=mid+1;
+            }
+        }
+        // last position
+        low=0, high=nums.size()-1;
+        while(low<=high) {
+            mid=low+(high-low)/2;
+            if(nums[mid]==target) {
+                lastPosition=mid;
+                low=mid+1;
+            } else if(nums[mid]>target) {
+                high=mid-1;
+            } else {
+                low=mid+1;
+            }
+        }
+        return {firstPosition, lastPosition};
+    }
+};
